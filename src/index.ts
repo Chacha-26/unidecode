91e5e8b521d5b303723aef42a39ca50196f6d0d3
Chacha-26 <chacha-26@protonmail.com>
2017-10-21 23:02:19 +0100

    Rename u2a to unidecode

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { data } from './data';
 
 const nonAscii = /[^\x00-\x7f]/gu;
 
-const u2a = (str: string): string => String(str).replace(nonAscii, convert);
+const unidecode = (str: string): string => String(str).replace(nonAscii, convert);
 
 const convert = (char: string): string => {
     const code = char.codePointAt(0);
@@ -11,4 +11,4 @@ const convert = (char: string): string => {
     return hi in data ? data[hi][lo] : '';
 };
 
-export default u2a;
+export default unidecode;
