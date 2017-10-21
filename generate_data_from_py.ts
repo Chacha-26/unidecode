9d2b259f3c7895731e12e2d85e29b1c2beffeb07
Chacha-26 <chacha-26@protonmail.com>
2017-10-21 23:00:52 +0100

    Remove option to generate multiple data files

--- generate_data_from_py.ts
+++ generate_data_from_py.ts
@@ -2,7 +2,6 @@
 const SRC_DIR = 'raw_data';
 const DST_DIR = 'generated';
 const SHIFT_LENGTH = true;
-const SUPPRESS_LOCAL = true;
 const FILE_FILTER = /^x[0-9a-f]{3}\.py$/;
 
 import * as fs from 'fs';
@@ -10,14 +9,12 @@ import * as fs from 'fs';
 // Format
 
 interface Info {
-    global_idxs: number[];
-    local_idxs: number[];
-    local_str: string;
+    indices: number[];
     lengths: number[];
 }
 
 const data = new Map<string, Info>();
-let global_str = '';
+let str = '';
 
 const files = fs.readdirSync(SRC_DIR);
 
@@ -29,87 +26,72 @@ for (const fname of files) {
 
         const lengths = parsed.map(char => char.length);
 
-        let local_str = '';
-        const local_idxs = parsed.map((char, idx) => {
-            let found = local_str.indexOf(char);
+        const indices = parsed.map((char, idx) => {
+            let found = str.indexOf(char);
             if (found < 0) {
-                found = local_str.length;
-                local_str += char;
+                found = str.length;
+                str += char;
             }
             return found;
         });
 
-        const global_idxs = parsed.map((char, idx) => {
-            let found = global_str.indexOf(char);
-            if (found < 0) {
-                found = global_str.length;
-                global_str += char;
-            }
-            return found;
-        });
-
-        data.set(fname.slice(0, 4), { global_idxs, local_idxs, local_str, lengths });
+        data.set(fname.slice(0, 4), { indices, lengths });
     }
 }
 
-let max_length = 0, global_max_index = 0, local_max_index = 0;
+let max_length = 0, max_index = 0;
 for (const val of data.values()) {
-    global_max_index = Math.max(global_max_index, ...val.global_idxs);
-    local_max_index = Math.max(local_max_index, ...val.local_idxs);
+    max_index = Math.max(max_index, ...val.indices);
     max_length = Math.max(max_length, ...val.lengths);
 }
 // max_index = 00000000 00000000 00000000 00001010
 // clz(...)  = 8 + 8 + 8 + 4 = 32 - 4;
 // 32 - ...  = 4;
 // ...       = number of bits needed
-const global_index_bits = 32 - Math.clz32(global_max_index);
-const local_index_bits = 32 - Math.clz32(local_max_index);
+const index_bits = 32 - Math.clz32(max_index);
 const length_bits = 32 - Math.clz32(max_length);
 
-if (global_index_bits + length_bits > 32) {
-    console.error(`${global_index_bits} + ${length_bits} = ${global_index_bits + length_bits} > 32.`);
+if (index_bits + length_bits > 32) {
+    console.error(`${index_bits} + ${length_bits} = ${index_bits + length_bits} > 32.`);
     process.exit(1);
 }
 
-const local_index_mask = (1 << local_index_bits) - 1;
-const local_index_shift = SHIFT_LENGTH ? 0 : local_index_bits;
-const global_index_mask = (1 << global_index_bits) - 1;
-const global_index_shift = SHIFT_LENGTH ? 0 : global_index_bits;
+const index_mask = (1 << index_bits) - 1;
+const index_shift = SHIFT_LENGTH ? 0 : index_bits;
 const length_mask = (1 << length_bits) - 1;
 const length_shift = SHIFT_LENGTH ? length_bits : 0;
-
-fs.writeFileSync(DST_DIR + '/constants.d.ts', `// This file was automatically generated. Changes will be undone.
-declare namespace U2A {
-    const enum $ {
-        LOCAL_INDEX_BITS = ${ local_index_bits },
-        LOCAL_INDEX_MASK = ${ local_index_mask },
-        LOCAL_INDEX_SHIFT = ${ local_index_shift },
-        GLOBAL_INDEX_BITS = ${ global_index_bits },
-        GLOBAL_INDEX_MASK = ${ global_index_mask },
-        GLOBAL_INDEX_SHIFT = ${ global_index_shift },
-        LENGTH_BITS = ${ length_bits },
-        LENGTH_MASK = ${ length_mask },
-        LENGTH_SHIFT = ${ length_shift },
-    }
-}
-`, { encoding: 'utf8' });
 
 let combined = Object.create(null);
 for (const [fname, val] of data) {
     const key = parseInt(fname.slice(1), 16);
-    combined[key] = val.local_idxs.map((idx, i) => (idx << global_index_shift) | (val.lengths[i] << length_shift))
-    const obj = [
-        val.local_str,
-        val.local_idxs.map((idx, i) => (idx << local_index_shift) | (val.lengths[i] << length_shift)),
-    ];
-    fs.writeFileSync(DST_DIR + '/' + fname + '.json', JSON.stringify(obj), { encoding: 'utf8' });
+    combined[key] = val.indices.map((idx, i) => (idx << index_shift) | (val.lengths[i] << length_shift));
 }
-fs.writeFileSync(DST_DIR + '/combined.json', `[
-    ${JSON.stringify(global_str)}
-,{${
-Object.keys(combined).map(key => `
-    "${key}": [${ combined[key] }]`)}
-}]`, { encoding: 'utf8' });
+
+fs.writeFileSync(DST_DIR + '/index.ts', `// This file was automatically generated. Changes will be undone.
+
+export const enum $ {
+    INDEX_BITS = ${ index_bits },
+    INDEX_MASK = ${ index_mask },
+    INDEX_SHIFT = ${ index_shift },
+    LENGTH_BITS = ${ length_bits },
+    LENGTH_MASK = ${ length_mask },
+    LENGTH_SHIFT = ${ length_shift },
+}
+
+export const str = 
+${
+    str.match(/.{1,101}/g).map(s => '    ' + JSON.stringify(s)).join(' +\n')
+};
+
+export const data = {${
+    // Object.keys sorts numeric keys numerically for us
+    Object.keys(combined)
+    .filter(key => combined[key].find(x => x > 0)) // Filter out completely empty sections
+    .map(key => '\n    0x' + ('00' + (+key).toString(16)).slice(-3) + ': [' + combined[key] + ']')
+}
+};
+
+`, { encoding: 'utf8' });
 
 process.exit(0);
 
