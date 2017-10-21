81072dcdb9abb4b14a7be5eeefe5496a5173dcde
Chacha-26 <chacha-26@protonmail.com>
2017-10-21 23:02:09 +0100

    Use arrow functions

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,13 @@ import { data } from './data';
 
 const nonAscii = /[^\x00-\x7f]/gu;
 
-function u2a(str: string): string {
-    return String(str).replace(nonAscii, convert);
-}
+const u2a = (str: string): string => String(str).replace(nonAscii, convert);
 
-function convert(char: string): string {
+const convert = (char: string): string => {
     const code = char.codePointAt(0);
     const hi = code >>> 8; // upper 24 bits
     const lo = code & 255; // lower 8 bits
     return hi in data ? data[hi][lo] : '';
-}
+};
 
 export default u2a;
