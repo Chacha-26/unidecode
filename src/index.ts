db3b8b3ee9e9e7d7f93f89a7b3b8086d5110a53c
Chacha-26 <chacha-26@protonmail.com>
2017-10-21 23:02:02 +0100

    Remove Cache

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,16 @@
 import { $, data, text } from './data';
 
 const nonAscii = /[^\x00-\x7f]/gu;
-let cache: Record<string, string> = Object.create(null);
-let cache_size = 0;
 
 function u2a(str: string): string {
     return String(str).replace(nonAscii, convert);
 }
 
 function convert(char: string): string {
-    if (char in cache) {
-        return cache[char];
-    }
-
-    // TODO: Better cache rotation
-    cache_size += 1;
-    if (cache_size > 100000) {
-        cache = {};
-        cache_size = 0;
-    }
-
     const code = char.codePointAt(0);
     const hi = code >>> 8; // upper 24 bits
     const lo = code & 255; // lower 8 bits
-    return cache[char] = hi in data ? unpack(data[hi][lo]) : '';
+    return hi in data ? unpack(data[hi][lo]) : '';
 }
 
 function unpack(code: number): string {
